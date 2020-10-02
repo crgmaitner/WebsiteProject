8f0392d716572ccad240a7752f1d499c44725c1d
crgmaitner <crgmaitner@users.noreply.github.com>
2020-10-02 15:27:50 -0400

    server constructed

--- server/siteServer.js
+++ server/siteServer.js
@@ -4,4 +4,4 @@ const port = 1337
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Server listening on port ${port}.`))
