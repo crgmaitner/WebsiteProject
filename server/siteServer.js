b1dfdc74c6a0897e71170701c5036f472cbb9430
crgmaitner <crgmaitner@users.noreply.github.com>
2020-10-03 23:38:58 -0400

    Begun building out site entrance page.

--- server/siteServer.js
+++ server/siteServer.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 1337
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', function (req, res) {
+    res.sendFile('index.html', {root: './client/views'})
+})
 
 app.listen(port, () => console.log(`Server listening on port ${port}.`))
