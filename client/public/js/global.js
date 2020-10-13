907f7095d17ce438dfc584c9ad3afc8a593a849c
crgmaitner <crgmaitner@users.noreply.github.com>
2020-10-13 11:58:00 -0400

    Core site layout complete. Begin page population.

---
+++ client/public/js/global.js
@@ -0,0 +1,4 @@
+//This function is run whenever a page moves to another location.
+function goToLocation(location) {
+	window.location = location;
+}
--- server/siteServer.js
+++ server/siteServer.js
@@ -7,4 +7,20 @@ app.get('/', function (req, res) {
     res.sendFile('index.html', {root: './client/views'})
 })
 
+app.get('/shop', function (req, res) {
+    res.sendFile('shop.html', {root: './client/views'})
+})
+
+app.get('/account', function (req, res) {
+    res.sendFile('account.html', {root: './client/views' })
+})
+
+app.get('/about', function (req, res) {
+    res.sendFile('about.html', {root: './client/views'})
+})
+
+app.get('/cart', function (req, res) {
+    res.sendFile('cart.html', {root: './client/views'})
+})
+
 app.listen(port, () => console.log(`Server listening on port ${port}.`))
